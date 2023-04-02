a767aba3a39a3df54140b3cf604275fd5e18b534
ywilfredo <74474078+ywilfredo@users.noreply.github.com>
2023-04-02 12:27:15 -0400

    Modificar el Program.cs para agregar como servicio a DbContext

--- TecnoShop/Program.cs
+++ TecnoShop/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using TecnoShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+//Agregar como servicio al dbcontext
+builder.Services.AddDbContext<TecnoShopDbContext>(options => {
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:Conexion"]);
+});
 
 var app = builder.Build();
 

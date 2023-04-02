ccffde1ef2b480bbd69842525be6096e7aa2acf0
ywilfredo <74474078+ywilfredo@users.noreply.github.com>
2023-04-02 19:36:25 -0400

    Agregar CategoriaController y MarcaController

---
+++ TecnoShop/Controllers/CategoriaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TecnoShop.Models;
+using TecnoShop.ViewModels;
+
+namespace TecnoShop.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ICategoriaRepositorio _categoriaRepositorio;
+
+        public CategoriaController(ICategoriaRepositorio categoriaRepositorio)
+        {
+            _categoriaRepositorio = categoriaRepositorio;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListaCategoria()
+        {
+
+            ListaCategoriaViewModel listaCategoriaViewModel = new ListaCategoriaViewModel(_categoriaRepositorio.TodasLasCategorias);
+            return View(listaCategoriaViewModel);
+        }
+    }
+}
---
+++ TecnoShop/Controllers/MarcaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TecnoShop.Models;
+using TecnoShop.ViewModels;
+
+namespace TecnoShop.Controllers
+{
+    public class MarcaController : Controller
+    {
+        private readonly IMarcaRepositorio _marcaRepositorio;
+
+        public MarcaController(IMarcaRepositorio marcaRepositorio)
+        {
+            _marcaRepositorio = marcaRepositorio;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListaMarca()
+        {
+
+            ListaMarcaViewModel listaMarcaViewModel = new ListaMarcaViewModel(_marcaRepositorio.TodasLasMarcas);
+            return View(listaMarcaViewModel);
+        }
+    }
+}

9f505e52027e7650213f3bb4d03ed090d41e1315
ywilfredo <74474078+ywilfredo@users.noreply.github.com>
2023-04-02 17:22:20 -0400

    Agregar ViewModels y actualizar _ViewImports.cshtml

---
+++ TecnoShop/ViewModels/ListaProductoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.IO.Pipelines;
+using TecnoShop.Models;
+namespace TecnoShop.ViewModels
+{
+    public class ListaProductoViewModel
+    {
+        public IEnumerable<Producto> Productos { get; }
+        public string? CategoriaActual { get; }
+
+        public ListaProductoViewModel(IEnumerable<Producto> productos, string? categoriaActual)
+        {
+            Productos = productos;
+            CategoriaActual = categoriaActual;
+        }
+    }
+}

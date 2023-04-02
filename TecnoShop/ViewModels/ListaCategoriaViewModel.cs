77f644cb4566078b5159db25880acc40431ec877
ywilfredo <74474078+ywilfredo@users.noreply.github.com>
2023-04-02 19:27:57 -0400

    Agregar ViewModel de Categoria y Marca

---
+++ TecnoShop/ViewModels/ListaCategoriaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using TecnoShop.Models;
+
+namespace TecnoShop.ViewModels
+{
+    public class ListaCategoriaViewModel
+    {
+        public IEnumerable<Categoria> Categorias { get; }
+
+        public ListaCategoriaViewModel(IEnumerable<Categoria> categorias)
+        {
+            Categorias = categorias;
+        }
+    }
+}
---
+++ TecnoShop/ViewModels/ListaMarcaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using TecnoShop.Models;
+
+namespace TecnoShop.ViewModels
+{
+    public class ListaMarcaViewModel
+    {
+        public IEnumerable<Marca> Marcas { get; }
+
+        public ListaMarcaViewModel(IEnumerable<Marca> marcas)
+        {
+            Marcas = marcas;
+        }
+    }
+}

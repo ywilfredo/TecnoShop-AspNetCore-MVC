26686e32e5f40d3d77f3c7506e7360785df88b69
ywilfredo <74474078+ywilfredo@users.noreply.github.com>
2023-04-02 17:40:46 -0400

    Agregar Controlador de Producto

---
+++ TecnoShop/Controllers/ProductoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TecnoShop.Models;
+using TecnoShop.ViewModels;
+
+namespace TecnoShop.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly IProductoRepositorio _productoRepositorio;
+        private readonly ICategoriaRepositorio _categoriaRepositorio;
+        private readonly IMarcaRepositorio _marcaRepositorio;
+
+        public ProductoController(IProductoRepositorio productoRepositorio, ICategoriaRepositorio categoriaRepositorio, IMarcaRepositorio marcaRepositorio)
+        {
+            _productoRepositorio = productoRepositorio;
+            _categoriaRepositorio = categoriaRepositorio;
+            _marcaRepositorio = marcaRepositorio;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        public IActionResult ListarProductos()
+        {
+
+            ListaProductoViewModel listaProductoViewModel = new ListaProductoViewModel(_productoRepositorio.TodosLosProductos, "Gaming");
+            return View(listaProductoViewModel);
+        }
+    }
+}

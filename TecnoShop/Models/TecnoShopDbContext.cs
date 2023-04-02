5dfd4d90f59b11117ff8971ced868157256e12dc
ywilfredo <74474078+ywilfredo@users.noreply.github.com>
2023-04-02 11:59:42 -0400

    Agregar clase DbContext

---
+++ TecnoShop/Models/TecnoShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace TecnoShop.Models
+{
+    public class TecnoShopDbContext : DbContext
+    {
+        public TecnoShopDbContext(DbContextOptions<TecnoShopDbContext> options) : base(options)
+        {
+
+        }
+
+        //Estas son las tablas que van a estar en la base de datos: tiene que ser del tipo de la clase y en Plural para Las tablas
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+
+    }
+}

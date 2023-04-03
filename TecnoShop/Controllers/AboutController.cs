fd66dafa7ec7b66170a5baac4592702c9dbf2431
ywilfredo <74474078+ywilfredo@users.noreply.github.com>
2023-04-02 22:46:40 -0400

    Agregar pagina About us

---
+++ TecnoShop/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TecnoShop.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
